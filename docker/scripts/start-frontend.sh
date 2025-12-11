0b106d3dc828aa60293ceef7b073be07bf5bf48c
roka <sarki_roka@freemail.hu>
2025-12-11 20:43:25 +0100

    fix: add lightningcss native bindings, update start script

--- docker/scripts/start-frontend.sh
+++ docker/scripts/start-frontend.sh
@@ -3,10 +3,14 @@
 cat /usr/local/app/docker/conf/frontend-art.txt
 
 echo ""
+echo "Cleaning cache..."
+rm -rf .next
+rm -f package-lock.json
+
+echo ""
 echo "Installing dependencies..."
 npm install
 
 echo ""
 echo "Starting Next.js development server..."
 npm run dev
-
